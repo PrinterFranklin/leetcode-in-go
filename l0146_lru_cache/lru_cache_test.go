30dfa982be8bed8afecb05a89f575d953e50cb5b
Tianxing Zhang <nbztx@126.com>
2021-08-30 07:30:59 +0800

    146: LRU Cache #1

---
+++ l0146_lru_cache/lru_cache_test.go
@@ -0,0 +1,27 @@
+package l0146_lru_cache
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCase_1(t *testing.T) {
+	cache := Constructor(2)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+	assert.Equal(t, 1, cache.Get(1)) // returns 1
+
+	cache.Put(3, 3)                   // evicts key 2
+	assert.Equal(t, -1, cache.Get(2)) // returns -1 (not found)
+
+	cache.Put(4, 4)                   // evicts key 1
+	assert.Equal(t, -1, cache.Get(1)) // returns -1 (not found)
+	assert.Equal(t, 3, cache.Get(3))  // returns 3
+	assert.Equal(t, 4, cache.Get(4))  // returns 4
+}
+
+func TestCase_2(t *testing.T) {
+	cache := Constructor(1)
+	cache.Put(2, 1)
+	assert.Equal(t, 1, cache.Get(2))
+}
