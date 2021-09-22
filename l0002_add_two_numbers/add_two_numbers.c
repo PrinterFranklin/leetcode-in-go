e24175d9641b11ff0946a69b51689fb560867a7e
Tianxing Zhang <anakinzhang96@gmail.com>
2021-09-22 08:09:00 +0800

    l0002: add two numbers

---
+++ l0002_add_two_numbers/add_two_numbers.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy = {
+        .val = 0,
+        .next = NULL,
+    };
+    struct ListNode *tail = &dummy;
+    int carry = 0;
+    while (l1 || l2) {
+        int n1 = l1 ? l1->val : 0;
+        int n2 = l2 ? l2->val : 0;
+        int sum = n1 + n2 + carry;
+
+        tail->next = malloc(sizeof(struct ListNode));
+        tail = tail->next;
+        tail->val = sum % 10;
+        tail->next = NULL;
+        carry = sum / 10;
+
+        if (l1)
+            l1 = l2->next;
+        if (l2)
+            l2 = l2->next;
+    }
+    if (carry > 0) {
+        tail->next = malloc(sizeof(struct ListNode));
+        tail->next->val = carry;
+        tail->next->next = NULL;
+    }
+    return dummy.next;
+}
