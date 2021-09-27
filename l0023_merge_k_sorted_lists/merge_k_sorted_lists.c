505b92d6f314d42356dd8f3c02b35c6ee0f72574
Tianxing Zhang <anakinzhang96@gmail.com>
2021-09-27 08:35:50 +0800

    l0023: merge k sorted lists

---
+++ l0023_merge_k_sorted_lists/merge_k_sorted_lists.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* mergeTwoLists(struct ListNode *l1, struct ListNode *l2) {
+    if (l1 == NULL)
+        return l2;
+    if (l2 == NULL)
+        return l1;
+    if (l1->val < l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
+
+struct ListNode* TwoLists(struct ListNode** lists, int left, int right) {
+    if (left == right)
+        return lists[left];
+    if (left > right)
+        return NULL;
+    int mid = left + (right - left) / 2;
+    struct ListNode *l1 = TwoLists(lists, left, mid);
+    struct ListNode *l2 = TwoLists(lists, mid+1, right);
+    return mergeTwoLists(l1, l2);
+}
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    if (!listsSize)
+        return NULL;
+    return TwoLists(lists, 0, listsSize-1);
+}
---
+++ l0023_merge_k_sorted_lists/merge_k_sorted_lists.go
@@ -0,0 +1,36 @@
+package leetcode0023
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	length := len(lists)
+	if length < 1 {
+		return nil
+	}
+	if length == 1 {
+		return lists[0]
+	}
+	num := length / 2
+	left := mergeKLists(lists[:num])
+	right := mergeKLists(lists[num:])
+	return mergeTwoLists(left, right)
+}
