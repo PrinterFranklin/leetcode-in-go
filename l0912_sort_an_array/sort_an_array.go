3ca7273ef9e215b65d125b5bfd948dd6be10c237
PrinterFranklin <nbztx@126.com>
2021-10-09 15:07:15 +0800

    l0912: merge sort, bubble sort, selection sort and insertion sort

--- l0912_sort_an_array/sort_an_array.go
+++ l0912_sort_an_array/sort_an_array.go
@@ -29,7 +29,7 @@ func sortArray(nums Ints) []int {
 	return nums
 }
 
-// Solution 1: Quick Sort
+// Solution 1: Quick Sort (fast)
 // Time: O(NlogN)
 // Space: O(1)
 // Stable: no
@@ -67,7 +67,7 @@ func partition(nums []int, lo, hi int) int {
 	return i
 }
 
-// Solution 2: Heap Sort
+// Solution 2: Heap Sort (fast)
 // Time: O(NlogN)
 // Space: O(1)
 // Stable: no
@@ -84,20 +84,82 @@ func sink(nums []int, i, len int) {
 
 }
 
-// Solution 3: Merge Sort
+// Solution 3: Merge Sort (fast)
 // Time: O(NlogN)
 // Space: O(N)
 // Stable: yes
 func sortArray3(nums []int) []int {
-	return nil
+	mergeSort(nums, 0, len(nums)-1)
+	return nums
 }
 
-// Solution 4: Insertion Sort
+func mergeSort(nums []int, l, r int) {
+	if l < r {
+		m := (l + r) / 2
+		mergeSort(nums, l, m)
+		mergeSort(nums, m+1, r)
+		merge(nums, l, m, r)
+	}
+}
+
+func merge(nums []int, l, m, r int) {
+	// Step 1: create two temp arrays
+	n1, n2 := m-l+1, r-m
+
+	left := make([]int, n1)
+	right := make([]int, n2)
+
+	// Step 2: write elements to temp arrays
+	for i := 0; i < n1; i++ {
+		left[i] = nums[l+i]
+	}
+	for j := 0; j < n2; j++ {
+		right[j] = nums[m+1+j]
+	}
+
+	// initial indexes
+	i, j, k := 0, 0, l
+	// Step 3: write back the array elements in order
+	for i < n1 && j < n2 {
+		if left[i] < right[j] {
+			nums[k] = left[i]
+			i++
+		} else {
+			nums[k] = right[j]
+			j++
+		}
+		k++
+	}
+	// Step 4: write back the remaining elements
+	for i < n1 {
+		nums[k] = left[i]
+		i++
+		k++
+	}
+	for j < n2 {
+		nums[k] = right[j]
+		j++
+		k++
+	}
+}
+
+// Solution 4: Insertion Sort (slow)
 // Time: O(N^2)
 // Space: O(1)
 // Stable: yes
 func sortArray4(nums []int) []int {
-	return nil
+	// Loop 1: iterate unsorted array
+	for i := 1; i < len(nums); i++ {
+		tmp := nums[i]
+		j := i - 1
+		// Loop 2: compare and move
+		for j >= 0 && nums[j] > tmp {
+			nums[j+1] = nums[j]
+			j--
+		}
+		nums[j+1] = tmp
+	}
+	return nums
 }
 
 // Solution 5: Bubble Sort (too slow)
@@ -117,12 +179,23 @@ func sortArray5(nums []int) []int {
 	return nums
 }
 
-// Solution 6: Selection Sort
+// Solution 6: Selection Sort (too slow)
 // Time: O(N^2)
 // Space: O(1)
 // Stable: no
 func sortArray6(nums []int) []int {
-	return nil
+	// Loop 1: swap the minimum element to the tail of a sorted array
+	for i := 0; i < len(nums); i++ {
+		minIdx := i
+		// Loop 2: find the minimum element index
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[minIdx] {
+				minIdx = j
+			}
+		}
+		nums[i], nums[minIdx] = nums[minIdx], nums[i]
+	}
+	return nums
 }
 
 // Solution 7: Shell Sort
