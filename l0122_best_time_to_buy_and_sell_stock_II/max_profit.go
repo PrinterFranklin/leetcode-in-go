184df60444e01e1f1146fe153915767eec1d76e5
PrinterFranklin <nbztx@126.com>
2021-10-20 14:49:28 +0800

    l0122: best time to buy stock

--- l0122_best_time_to_buy_and_sell_stock_II/max_profit.go
+++ l0122_best_time_to_buy_and_sell_stock_II/max_profit.go
@@ -1,7 +1,15 @@
 package leetcode0122
 
+// Solution 1: Greedy
 func maxProfit(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
 	res := 0
-
+	for i := 1; i < len(prices); i++ {
+		if prices[i-1] < prices[i] {
+			res = res + prices[i] - prices[i-1]
+		}
+	}
 	return res
 }
