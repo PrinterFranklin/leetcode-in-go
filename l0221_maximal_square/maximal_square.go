f08ec6f86189a4dcb6da7817c00b8d8c3e3f571a
PrinterFranklin <nbztx@126.com>
2021-10-27 18:51:05 +0800

    l0221: maximal square

---
+++ l0221_maximal_square/maximal_square.go
@@ -0,0 +1,38 @@
+package leetcode0221
+
+// Solution 1: DP
+func maximalSquare(matrix [][]byte) int {
+	// dp: max side value of the rectangle
+	dp := make([][]int, len(matrix))
+	maxSide := 0
+	// init
+	for i := 0; i < len(matrix); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+		for j := 0; j < len(matrix[i]); j++ {
+			dp[i][j] = int(matrix[i][j] - '0')
+			if dp[i][j] == 1 {
+				maxSide = 1
+			}
+		}
+	}
+	// calculating max side for every rectangle
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[i]); j++ {
+			if dp[i][j] == 1 {
+				// key formula
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+				if dp[i][j] > maxSide {
+					maxSide = dp[i][j]
+				}
+			}
+		}
+	}
+	return maxSide * maxSide
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
