1e78ba2b47c08e901fe65d09d384c15a821ac4aa
PrinterFranklin <nbztx@126.com>
2021-11-06 15:26:49 +0800

    l0001: two sum (add test)

--- l0001_two_sum/two_sum_test.go
+++ l0001_two_sum/two_sum_test.go
@@ -1,1 +1,24 @@
 package leetcode0001
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoSum(t *testing.T) {
+	type testArr struct {
+		nums   []int
+		target int
+		result []int
+	}
+	a := []testArr{
+		{nums: []int{2, 7, 11, 15}, target: 9, result: []int{0, 1}},
+		{nums: []int{3, 2, 4}, target: 6, result: []int{1, 2}},
+		{nums: []int{3, 3}, target: 6, result: []int{0, 1}},
+	}
+	for _, v := range a {
+		re := twoSum(v.nums, v.target)
+		assert.Equal(t, v.result, re)
+	}
+}
